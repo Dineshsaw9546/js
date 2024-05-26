19412fbbb6df3414e7737a9fc92f0f90634f6d62
Dineshsaw9546 <vikeshsaw0@gmail.com>
2024-05-26 08:01:59 +0000

    dataType

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,22 @@
+const accountId = 144553
+let accountEmail = "dinesh@google.com"
+var accountPassword = "12345"
+accountCity ="gadi"
+
+
+//accountId =2// not allowed
+
+ 
+
+accountEmail ="hc@hc.com"
+accountPassword ="212111"
+accountCity = "tara"
+console.log(accountId);
+console.table([accountId,accountEmail, accountPassword,accountCity])
+
+
+/*
+  prefer not to use var 
+  because
+
+*/
---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,19 @@
+" USE STRICT"; // TREAT all js code a newer version
+
+//alert(3+3+) // we are usingnode js, not broweser 
+
+console.log(3+3);//code readability
+
+
+// number=> 2 to power 53
+// bigint 
+//string=>""
+//boolean => true/false
+// null => standalone value
+//undefined=> 
+    //symbol=>unique
+
+
+//object//
+let name = "dinesh saw"
+console.log(typeof null);
