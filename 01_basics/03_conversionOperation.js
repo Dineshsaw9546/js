5c33c11ed27a3a3a1c6a920b589693fcbfeab4dd
Dineshsaw9546 <vikeshsaw0@gmail.com>
2024-05-26 09:53:54 +0000

    dataType-summary

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,6 @@
+let str1 = "Hello" 
+let str2 = " yes"
+
+let str3= " str1+str2"
+console.log(str1 + str2);
+ 
---
+++ 01_basics/dataType-summary.js
@@ -0,0 +1,30 @@
+//primative
+
+// 7 types: string, Number, Boolean, null, undefinde, symbol, bigInt,
+
+
+//referance(non-primatave)
+// Array, Objects, Functions
+const score =100
+const scoreValue = 100.3
+
+const isLoggedIn = false
+const outsidetemp = null
+let userEmail;
+
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+const heros= [ "shaktiman", "naaqraj", "doga"];
+let myObj = {
+    name:"Dinesh",
+    age:21;
+}
+
+
+const myFunction = function(){
+    console.log("Hello world");
+}
+
+console.log(typeof scoreValue);
