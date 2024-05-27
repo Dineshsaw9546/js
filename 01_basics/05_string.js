93a7a2067e6699064b613acb52b846da17ea63e4
Dineshsaw9546 <vikeshsaw0@gmail.com>
2024-05-27 02:03:02 +0000

    maths and num

---
+++ 01_basics/05_string.js
@@ -0,0 +1,18 @@
+//const name = "dinesh"
+const repoCount = 50
+
+//console.log(name +repoCount + "value");
+ //console.log(` Hello my nameis ${name} and my repo count is ${repoCount}`);\
+
+ const gameName = new String(`dinesh`)
+
+ console.log((gameName[0]));
+ console.log(gameName._proto_ );
+
+ console.log(gameName.length);
+ console.log(gameName.toUpperCase());
+
+ const url = "https://dinesh.com/hitesh%20saw"
+ console.log(url.replace(`%20`, `-`));
+
+ console.log(url.includes('rab'));
---
+++ 01_basics/06_nums_and_maths.js
@@ -0,0 +1,21 @@
+//const score = 400
+
+//const balance =new Number (100)
+//console.log(balance);
+
+
+//console.log(balance.toString());
+//console.log(balance.toFixed(3));
+
+//const otherNumber = 23.4542
+//console.log(otherNumber.toPrecision(2));
+
+//const hundreds = 10000
+//console.log(hundreds.toLocaleString(`en-IN`));
+
+//++++++++++++++++++ Maths++++++++++
+//console.log(Math);
+//console.log(Maths.abs(-4));
+//console.log(Maths.round(4.3));
+
+console.log(Maths.random());
