38cdba57b3d75aea312a842fd83e1291ea763a81
Dineshsaw9546 <vikeshsaw0@gmail.com>
2024-05-31 03:31:59 +0000

    js

---
+++ 04_control_flow/one.js
@@ -0,0 +1,12 @@
+/// if
+
+ const userLoggedIn = true
+ const debitCard = true 
+
+ if ( userLoggedIn && debitCard && 2==3){
+    console.log("Allow to buy course");
+ }
+
+ if (loggedInFromGoogle || loggedInFromEmail){
+    console.log("userlogged in");
+ }
