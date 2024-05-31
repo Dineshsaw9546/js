d944f0b115215c11b76547996410e51e6f4072e9
Dineshsaw9546 <vikeshsaw0@gmail.com>
2024-05-31 13:24:32 +0000

    js.js

---
+++ 05_iterations/three.js
@@ -0,0 +1,20 @@
+//for of
+//const arr = [1,2,3,4]
+
+ //for (const num of arr) {
+   // console.log(num);
+ //}
+ 
+ //const greetings  = "Hello world!";
+ //for (const greet of greetings) {
+    //console.log(`Each char is $(great)`);
+ //}
+
+ //  maps 
+
+ const map = new Map()
+ map.set('IN', "India")
+ map.set('USA', "united state of America")
+ map.set('Fr', "France")
+
+ console.log(map);
